018f70e00c6d14e0f899a22f56aa1a8bfe616b83
Taehyun Kim <kkkk1598@naver.com>
2025-06-04 09:27:49 -0400

    Time: 1 ms (99.94%), Space: 61.6 MB (64.33%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -2,22 +2,22 @@ class Solution {
     public int maxProfit(int[] prices) {
         // cannot achieve any profit
         if(prices.length <= 1) return 0;
-        int i = 0;
+        int buyDay = 0;
         // base case
-        int maxSum = prices[1] - prices[0];
+        int maxProfit = prices[1] - prices[0];
 
         // using Sliding Window.
         for(int j = 1; j < prices.length; j++){
             // if jth price is smaller, then move the buying day
-            if(prices[j] <= prices[i]){
-                i = j;
+            if(prices[j] <= prices[buyDay]){
+                buyDay = j;
                 continue;
             } else{ // otherwise, compare with differnt day in the future
-                maxSum = Math.max(maxSum, prices[j] - prices[i]);
+                maxProfit = Math.max(maxProfit, prices[j] - prices[buyDay]);
             }
         }
 
         // return maximum profit from this transaction, if cannot achieve any profit, return 0.
-        return Math.max(maxSum,0);
+        return Math.max(maxProfit,0);
     }
 }
