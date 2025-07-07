2143429b62c269559f6b8ef8afed137cd58044b6
Taehyun Kim <kkkk1598@naver.com>
2025-07-07 08:55:02 -0400

    Time: 64 ms (89.04%), Space: 75.9 MB (23.78%) - LeetHub

---
+++ 1478-maximum-number-of-events-that-can-be-attended/1478-maximum-number-of-events-that-can-be-attended.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int maxEvents(int[][] events) {
+        int len = events.length;
+        int maxDay = 0;
+        // find the maximum end day
+        for(int[] event: events){
+            maxDay = Math.max(maxDay, event[1]);
+        }
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // sort its start days in ascending order
+        Arrays.sort(events, (a,b) -> a[0]- b[0]);
+        int ans = 0;
+        for(int i =1, j= 0; i<=maxDay; i++){
+            // store days by earlist end day
+            while(j < len && events[j][0] <= i){
+                pq.offer(events[j][1]);
+                j++; // increament j to avoid duplication and keep going
+            }
+            while(!pq.isEmpty() && pq.peek() < i){
+                pq.poll();
+            }
+            if(!pq.isEmpty()){
+                pq.poll();
+                ans++;
+            }
+
+        }
+
+        return ans;
+    }
+}
